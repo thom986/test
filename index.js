af0b89143f278678020521c280958f119212dafe
thom986 <66462203+thom986@users.noreply.github.com>
2021-02-08 19:18:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -276,37 +276,6 @@ async function promptMessage(message, author, time, reactions) {
 	message.channel.send(`Ik heb goednieuws, u gaat dood.`);
 } 
 	
-if (command === `${prefix}rps`) {
-        const acceptedReplies = ['steen', 'papier', 'schaar'];
-        const random = Math.floor((Math.random() * acceptedReplies.length));
-        const result = acceptedReplies[random];
-
-        const choice = !args[0];
-        if (!choice) return message.channel.send(`How to play: \`${prefix}rps <rock|paper|scissors>\``);
-        if (!acceptedReplies.includes(choice)) return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
-        
-        console.log('Bot Result:', result);
-        if (result === choice) return message.reply("It's a tie! We had the same choice.");
-        
-        switch (choice) {
-            case 'rock': {
-                if (result === 'paper') return message.reply('I won!');
-                else return message.reply('You won!');
-            }
-            case 'paper': {
-                if (result === 'scissors') return message.reply('I won!');
-                else return message.reply('You won!');        
-            }
-            case 'scissors': {
-                if (result === 'rock') return message.reply('I won!');
-                else return message.reply('You won!');
-            }
-            default: {
-                return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
-            }
-        }
-    }
-
 });
 
 

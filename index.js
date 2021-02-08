812155d891bd90a6f4b8a101bd60296e3e48f534
thom986 <66462203+thom986@users.noreply.github.com>
2021-02-08 18:59:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -276,44 +276,6 @@ async function promptMessage(message, author, time, reactions) {
 	message.channel.send(`Ik heb goednieuws, u gaat dood.`);
 } 
 
-if (command === `${prefix}rps`) {
-        if (!args[1]) {
-            return message.channel.send('Please include your choice.')
-        }
-
-        let choices = ['rock', 'paper', 'scissors'];
-        if (choices.includes((args[1]).toLowerCase())) {
-            let number = Math.floor(Math.random() * 3);
-            if (number == 1) {
-                return message.channel.send('It was a tie, we both had ' + (args[1]).toLowerCase())
-            }
-            if (number == 2) {
-                if ((args[1]).toLowerCase() == "rock") {
-                    return message.channel.send('I won, I had paper.')
-                }
-                if ((args[1]).toLowerCase() == "paper") {
-                    return message.channel.send('I won, I had scissors.')
-                }
-                if ((args[1]).toLowerCase() == "scissors") {
-                    return message.channel.send('I won, I rock.')
-                }
-            }
-            if (number == 0) {
-                if ((args[1]).toLowerCase() == "rock") {
-                    return message.channel.send('You won, I had scissors.')
-                }
-                if ((args[1]).toLowerCase() == "paper") {
-                    return message.channel.send('You won, I had rock.')
-                }
-                if ((args[1]).toLowerCase() == "scissors") {
-                    return message.channel.send('You won, I paper.')
-                }
-            }
-        } else {
-            return message.channel.send('Please include either: Rock, Paper, or Scissors.')
-        }
-    }
- 
 });
 
 

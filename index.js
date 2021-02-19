63f59687166c57e50a0c949671c08e4eedc63318
thom986 <66462203+thom986@users.noreply.github.com>
2021-02-19 10:23:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -278,40 +278,6 @@ async function promptMessage(message, author, time, reactions) {
 	message.channel.send(`https://discord.gg/yQBBbZPkD4`);
 }
 	
-	module.exports = {
-  name: "rps",
-  description: "A game of Rock, Paper, Scissors!",
-  execute(message) {
-    var rps = ["🗿", "📜", "✂️"]
-    const m = message.channel.send("Let's play a game of Rock, Paper, Scissors! Please react what you would like to choose with the emojis below!").then((message) => {
-      message.react("🗿");
-      message.react("📜");
-      message.react("✂️");
-    });
-    const reacted = promptMessage(m, message.author, 30, rps);
-
-    const botChoice = rps[Math.floor(Math.random()*rps.length)];
-    const result = getResult(reacted, botChoice);
-    m.clearReactions();
-
-    message.channel.send(`You chose ${reacted} and I chose ${botChoice}`);
-
-    function getResult(choice, botChosen) {
-      if(choice === "🗿" && botChoice === "✂️") {
-          return message.channel.send("You win! I had fun, let's play again!");
-        } else if (choice === "📜" && botChoice === "🗿") {
-          return message.channel.send("You win! I had fun, let's play again!");
-        } else if (choice === "✂️" && botChoice === "📜"){
-          return message.channel.send("You win! I had fun, let's play again!");
-        } else if (choice === botChosen) {
-          return message.channel.send("It's a tie!");
-        } else {
-          return message.channel.send("You lost! I had fun, let's play again!");
-        }
-    }
-  },
-};
-	
 });
 
 
